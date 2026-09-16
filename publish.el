94a874f18a27f0f6e5f8e90e55ebc57f4e4f681f
anevolbap <anevolbap@gmail.com>
2026-09-16 15:33:10 -0300

    feat: build resume PDF in CI and link it

--- publish.el
+++ publish.el
@@ -11,6 +11,25 @@
       ""
     "<nav><a href='/'>← Pablo Vena</a></nav>"))
 
+;; Resume PDF: keep the LaTeX packages to what texlive-latex-base and
+;; lmodern ship, so CI installs little.
+(setq org-latex-compiler "pdflatex"
+      org-latex-pdf-process
+      '("pdflatex -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -interaction nonstopmode -output-directory %o %f")
+      org-latex-default-packages-alist
+      '(("utf8" "inputenc" t) ("T1" "fontenc" t) ("" "lmodern" nil)
+        ("margin=2cm" "geometry" nil)
+        ("colorlinks=true,urlcolor={[rgb]{0.1,0.25,0.55}}" "hyperref" nil))
+      org-latex-packages-alist nil
+      org-latex-title-command "\\begin{center}{\\LARGE\\bfseries %t}\\end{center}")
+
+(defun site-remove-tex (_project)
+  "Remove the LaTeX build files that org leaves next to resume.org."
+  (dolist (f '("resume.tex" "resume.pdf"))
+    (let ((path (concat site-root "org/" f)))
+      (when (file-exists-p path) (delete-file path)))))
+
 (setq org-publish-project-alist
   `(("site-pages"
      :base-directory ,(concat site-root "org")
@@ -35,4 +54,16 @@
      :publishing-directory ,site-out
      :base-extension "css\\|pdf\\|png\\|jpg"
      :publishing-function org-publish-attachment)
-    ("site" :components ("site-pages" "site-static"))))
+    ("site-pdf"
+     :base-directory ,(concat site-root "org")
+     :publishing-directory ,site-out
+     :exclude ".*"
+     :include ("resume.org")
+     :publishing-function org-latex-publish-to-pdf
+     :completion-function site-remove-tex
+     :latex-header "\\setlength{\\parindent}{0pt}\\setlength{\\parskip}{4pt}\\let\\siteitemize\\itemize\\renewcommand{\\itemize}{\\siteitemize\\setlength{\\itemsep}{1pt}\\setlength{\\parskip}{0pt}}"
+     :with-toc nil
+     :section-numbers nil
+     :with-author nil
+     :with-date nil)
+    ("site" :components ("site-pages" "site-static" "site-pdf"))))
